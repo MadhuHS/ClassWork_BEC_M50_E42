cd4b8a146e69c865b4b9bad956bd20977e00ef5f
Madhuhs <madhuhs943@gmail.com>
2021-10-20 08:45:07 +0530

    jdbc PreparedStatement, insert updated

--- Concepts/src/com/jspiders/jdbc/Mainclass.java
+++ Concepts/src/com/jspiders/jdbc/Mainclass.java
@@ -2,6 +2,7 @@ package com.jspiders.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Mainclass 
@@ -14,24 +15,36 @@ public class Mainclass
 		
 		Connection con = null;
 		
-		//Step 1: Register and  Open Connection
+		String insertStmt = "insert into userdb.appusers values (?,?,?,?)";
+		
+		//Step 1: Register and Open Connection
 		try 
 		{
 			con = DriverManager.getConnection(dburl,dbUsername, dbPasssword);
 			System.out.println("Connected to userdb");
+			
+			//Step 2: Create
+			PreparedStatement pstmt = con.prepareStatement(insertStmt);
+			
+			pstmt.setInt(1, 0);
+			pstmt.setString(2, "user7");
+			pstmt.setString(3, "user8@gmail.com");
+			pstmt.setString(4, "98765432710");
+			System.out.println(pstmt);
+			
+			//Step 3: Execute
+			pstmt.executeUpdate();
+			
+			System.out.println("Record added to appuser table");
+			
 		}
 		catch (SQLException e)
 		{
 			e.printStackTrace();
 		}
+	
 		
-		//Step 2: Create
-		
-		//Step 3: Execute
-		
-		//Step 4: Process
-		
-		//Step 5: Close Connection
+		//Step 4: Close Connection
 		try
 		{
 			con.close();
