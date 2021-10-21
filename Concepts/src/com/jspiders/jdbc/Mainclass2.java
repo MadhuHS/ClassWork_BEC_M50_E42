b253304d67623eb3efcb58a27e1610ae69bdc495
Madhuhs <madhuhs943@gmail.com>
2021-10-21 08:39:28 +0530

    Adding selectAllUsers

--- Concepts/src/com/jspiders/jdbc/Mainclass2.java
+++ Concepts/src/com/jspiders/jdbc/Mainclass2.java
@@ -3,10 +3,10 @@ package com.jspiders.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-class DbDemo 
-{
+class DbDemo {
 	String dburl = "jdbc:mysql://localhost:3306/userdb";
 	String dbUsername = "root";
 	String dbPasssword = "root";
@@ -15,22 +15,20 @@ class DbDemo
 
 	String insertStmt = "insert into userdb.appusers values (?,?,?,?)";
 
-
 	// Step 1: Register and Open Connection
 	public void openConnectionToDb() {
-		try 
-		{
+		try {
 			con = DriverManager.getConnection(dburl, dbUsername, dbPasssword);
 			System.out.println("Connected to userdb");
-		} 
-		catch (SQLException e)
-		{
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void insert(String name, String email, String mob) {
 		try {
+
+			openConnectionToDb();
 			// Step 2: Create
 			PreparedStatement pstmt = con.prepareStatement(insertStmt);
 
@@ -43,53 +41,100 @@ class DbDemo
 			pstmt.executeUpdate();
 
 			System.out.println("Record added to appuser table");
-
-		} 
-		catch (SQLException e) 
-		{
+		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			closeConnection();
 		}
 
 	}
 
-	public void update(String name, String email, String mob)
-	{
-     String updateStmt = "update userdb.appusers set name = ?,email = ? where mob = ?";
+	public void update(String name, String email, String mob) {
+		String updateStmt = "update userdb.appusers set name = ?,email = ? where mob = ?";
 
-	  try 
-	  {
-		PreparedStatement pstmt = con.prepareStatement(updateStmt);
-		pstmt.setString(1, name);
-		pstmt.setString(2, email);
-		pstmt.setString(3, mob);
-		
-		int count = pstmt.executeUpdate();
-		System.out.println(count+" no of rows updated");
-	  } 
-	   
-	  catch (SQLException e)
-	  {
-		e.printStackTrace();
-	  }
+		try {
+			openConnectionToDb();
+			PreparedStatement pstmt = con.prepareStatement(updateStmt);
+			pstmt.setString(1, name);
+			pstmt.setString(2, email);
+			pstmt.setString(3, mob);
+
+			int count = pstmt.executeUpdate();
+			System.out.println(count + " no of rows updated");
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection();
+		}
 	}
 
-	public void deleteByEmail(String email)
+	public void deleteByEmail(String email) {
+		String deleteStmt = "delete from userdb.appusers where email = ?";
+
+		try {
+			openConnectionToDb();
+			PreparedStatement pstmt = con.prepareStatement(deleteStmt);
+			pstmt.setString(1, email);
+
+			int count = pstmt.executeUpdate();
+			System.out.println(count + " records deleted from appuser table");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeConnection();
+		}
+
+	}
+
+	public void selectUserByEmail(String email)
 	{
+
+	}
+
+	public void selectAllUsers() 
+	{
+       String selectAllStmt = "select * from userdb.appusers";
        
+       try 
+       {
+    	openConnectionToDb();
+		PreparedStatement pstmt = con.prepareStatement(selectAllStmt);
+		
+		ResultSet rSet  = pstmt.executeQuery(selectAllStmt);
+		
+		int count = 0;
+		
+		while(rSet.next() == true)
+		{
+			
+			int uid = rSet.getInt("uid");
+			String name = rSet.getString("name");
+			String email = rSet.getString("email");
+			String mob = rSet.getString("mob");
+			
+			System.out.println(uid+" "+name+" "+email+" "+mob);
+	
+			count++;
+		}
+		System.out.println(count+" records selected");
+	   }
+       catch (SQLException e)
+       {
+		e.printStackTrace();
+	   }
+       finally
+       {
+    	   closeConnection();
+       }
 	}
 
-	public void selectUserByEmail(String email) {
+	// Step 4: Close Connection
+	public void closeConnection() 
+	{
 
-	}
-
-	public void selectAllUsers() {
-
-	}
-
-	//Step 4: Close Connection
-	public void closeConnection() {
-
-		try
+		try 
 		{
 			con.close();
 			System.out.println("Disconnected from userdb");
@@ -101,24 +146,18 @@ class DbDemo
 	}
 }
 
-public class Mainclass2
+public class Mainclass2 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-       DbDemo d1 = new DbDemo();
-       
-       d1.openConnectionToDb();
-       
-       //d1.insert("user9", "user9@gmail.com", "9876543210");
-       d1.update("Appuser2","appuser2@gmail.com", "987766543211");
-      
-       d1.closeConnection();
+		DbDemo d1 = new DbDemo();
+
+		// d1.insert("user9", "user9@gmail.com", "9876543210");
+		// d1.update("Appuser2","appuser2@gmail.com", "987766543211");
+
+		//d1.deleteByEmail("user9@gmail.com");
+		
+		d1.selectAllUsers();
+
 	}
 }
-
-
-
-
-
-
-
